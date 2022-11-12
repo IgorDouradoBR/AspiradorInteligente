18171a5fa8d7d861f39708f5981991fda16f8703
Igor Pereira Dourado <70119242+IgorDouradoBR@users.noreply.github.com>
2022-11-12 17:46:24 -0300

    Add files via upload

---
+++ Q1interativo.py
@@ -0,0 +1,33 @@
+largura = float(input("Largura da sala: "))
+comprimento = float(input("Comprimento da sala: "))
+historico = input("Histórico de movimentação: ")
+cartografia = { 90: "O",  180 : "N" , 270 : "L",  0: "S"}
+posicao = 180
+coordX = 0
+coordY = 0
+
+for i in range(len(historico)):
+    if historico[i]=='D':
+        posicao += 90
+    if historico[i] == 'E':
+        posicao -= 90
+    if historico[i] == 'F':
+        if cartografia[posicao%360] == "N" and (coordY+1)< comprimento:
+            coordY += 1
+        if cartografia[posicao%360] == "S" and (coordY-1) >= 0.0:
+            coordY -= 1
+        if cartografia[posicao%360] == "L" and (coordX+1)< largura:
+            coordX += 1
+        if cartografia[posicao%360] == "O" and (coordX-1) >= 0.0:
+            coordX -= 1
+    if historico[i] == 'T':
+        if cartografia[posicao%360] == "N" and (coordY-1) >= 0.0:
+            coordY -= 1
+        if cartografia[posicao%360] == "S" and (coordY+1)< comprimento:
+            coordY += 1
+        if cartografia[posicao%360] == "L" and (coordX-1) >= 0.0:
+            coordX -= 1
+        if cartografia[posicao%360] == "O" and (coordX+1)< largura:
+            coordX += 1
+
+print (f'{cartografia[posicao%360]} {coordX} {coordY}')
